63257d259bc632e2f37393a43b6c3a51a7bb2cb9
matheusmelchiades <matheusmacielmelchiades@hotmail.com>
2019-05-02 04:43:21 -0300

    config init app

---
+++ src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const Main = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
+  </View>
+);
+
+export default Main;
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  welcome: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
